ad2f1f4b7295373aff4429880572a5e8eeafa51b
Gershon Tadepalli <gershon.tadepalli@toyota.com>
2020-06-19 00:37:55 +0530

    New Journey startedgit statusgit status!

---
+++ DS-AlgoPractice/DS-AlgoPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_AlgoPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = {1, 2, 3, 9, 8, 7};
+            TrapWater trapWater = new TrapWater();
+            int trappedWater = trapWater.Trap(height);
+            Console.WriteLine(trappedWater);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DS-AlgoPractice/DS-AlgoPractice/TrapWater.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_AlgoPractice
+{
+    internal class TrapWater
+    {
+        internal int Trap(int[] height)
+        {
+            int low = 0, high = height.Length - 1;
+            int leftMax = 0,rightMax = 0;
+            int res = 0;
+            while (low<high)
+            {
+                //move from left if lower
+                if(height[low]<height[high])
+                {
+                    //check if value is greater than leftMax or lower to fill water
+                    if (height[low] > leftMax) leftMax = height[low];
+                    else res += leftMax - height[low];
+                    low++;
+                }
+                else
+                {
+                    if (height[high] > rightMax) rightMax = height[high];
+                    else res += rightMax - height[high];
+                    high--;
+                }
+            }
+            return res;
+        }
+    }
+}
